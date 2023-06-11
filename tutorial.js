b3ec6c64635b61bdb6cf6d84e60cf7055487cafa
ak_131 <chixitokirin@gmail.com>
2023-06-11 11:57:49 +0900

    typescriptの型について学習しました

---
+++ tutorial.js
@@ -0,0 +1,6 @@
+var a = 3;
+var b = a;
+console.log(b);
+var array1 = [1, 2, 3, 4, 5];
+var array2 = [1, 2, 3, 4, 5];
+var array3 = array2;
---
+++ tutorial.ts
@@ -0,0 +1,9 @@
+const a: Number = 3;
+const b: Number = a;
+console.log(b);
+
+let array1: Array<Number> = [1,2,3,4,5];
+let array2: Number[] = [1,2,3,4,5];
+let array3: Number[] = array2;
+
+
